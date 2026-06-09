89f651a650b2cdaac00f5fc0fafe01f8bbcf58a3
Ivan12390123 <i_van_r@mail.ru>
2026-06-10 00:39:59 +0300

    fix adapter: improve security, combine Save and Find into one atomic method

--- internal/adapter/memory/repository.go
+++ internal/adapter/memory/repository.go
@@ -13,22 +13,29 @@ type InMemoryRepository struct {
 	byOriginalURL map[string]model.URL
 }
 
-func New() *InMemoryRepository {
+func NewRepo() *InMemoryRepository {
 	return &InMemoryRepository{
 		byShortCode:   make(map[string]model.URL),
 		byOriginalURL: make(map[string]model.URL),
 	}
 }
 
-func (r *InMemoryRepository) FindByOriginalURL(ctx context.Context, originalURL string) (*model.URL, error) {
-	r.mu.RLock()
-	defer r.mu.RUnlock()
+func (r *InMemoryRepository) CreateOrFind(ctx context.Context, urlInput *model.URL) (*model.URL, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
 
-	if url, exists := r.byOriginalURL[originalURL]; exists {
-		return url.Clone(), nil
+	if url, exists := r.byOriginalURL[urlInput.Original]; exists {
+		return &url, nil
 	}
 
-	return nil, model.ErrURLNotFound
+	if _, exists := r.byShortCode[urlInput.ShortCode]; exists {
+		return nil, model.ErrShortCodeExists
+	}
+
+	r.byShortCode[urlInput.ShortCode] = *urlInput
+	r.byOriginalURL[urlInput.Original] = *urlInput
+
+	return urlInput, nil
 }
 
 func (r *InMemoryRepository) FindByShortCode(ctx context.Context, shortCode string) (*model.URL, error) {
@@ -36,31 +43,8 @@ func (r *InMemoryRepository) FindByShortCode(ctx context.Context, shortCode stri
 	defer r.mu.RUnlock()
 
 	if url, exists := r.byShortCode[shortCode]; exists {
-		return url.Clone(), nil
+		return &url, nil
 	}
 
 	return nil, model.ErrURLNotFound
-}
-
-func (r *InMemoryRepository) Save(ctx context.Context, url *model.URL) error {
-	r.mu.Lock()
-	defer r.mu.Unlock()
-
-	if url == nil {
-		return model.ErrInvalidOriginalURL
-	}
-
-	if _, exists := r.byOriginalURL[url.Original]; exists {
-		return model.ErrOriginalURLExists
-	}
-
-	if _, exists := r.byShortCode[url.ShortCode]; exists {
-		return model.ErrShortCodeExists
-	}
-
-	stored := *url
-	r.byOriginalURL[url.Original] = stored
-	r.byShortCode[url.ShortCode] = stored
-
-	return nil
 }
--- internal/adapter/postgres/repository.go
+++ internal/adapter/postgres/repository.go
@@ -1,1 +1,94 @@
 package postgres
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/I-Van-Radkov/url-shortener/internal/model"
+	"github.com/Masterminds/squirrel"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type PostgresRepository struct {
+	db      *pgxpool.Pool
+	builder squirrel.StatementBuilderType
+}
+
+func NewRepo(db *pgxpool.Pool) *PostgresRepository {
+	return &PostgresRepository{
+		db:      db,
+		builder: squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar),
+	}
+}
+
+func (r *PostgresRepository) CreateOrFind(ctx context.Context, urlInput *model.URL) (*model.URL, error) {
+	query := `
+        INSERT INTO urls (short_code, original, created_at)
+        VALUES ($1, $2, $3)
+        ON CONFLICT (original) DO NOTHING
+        RETURNING short_code, original, created_at
+    `
+
+	var url model.URL
+	err := r.db.QueryRow(ctx, query, urlInput.ShortCode, urlInput.Original, urlInput.CreatedAt).Scan(&url.ShortCode, &url.Original, &url.CreatedAt)
+	if err == nil {
+		return &url, nil
+	}
+	if errors.Is(err, pgx.ErrNoRows) {
+		return r.findByOriginalURL(ctx, urlInput.Original)
+	}
+
+	var pgErr *pgconn.PgError
+	if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+		return nil, model.ErrShortCodeExists
+	}
+
+	return nil, fmt.Errorf("failed to create or find: %w", err)
+}
+
+func (r *PostgresRepository) FindByShortCode(ctx context.Context, shortCode string) (*model.URL, error) {
+	query, args, err := r.builder.
+		Select("short_code", "original", "created_at").
+		From("urls").
+		Where(squirrel.Eq{"short_code": shortCode}).
+		ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build query: %w", err)
+	}
+
+	var url model.URL
+	err = r.db.QueryRow(ctx, query, args...).Scan(&url.ShortCode, &url.Original, &url.CreatedAt)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, model.ErrURLNotFound
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute query: %w", err)
+	}
+
+	return &url, nil
+}
+
+func (r *PostgresRepository) findByOriginalURL(ctx context.Context, originalURL string) (*model.URL, error) {
+	query, args, err := r.builder.
+		Select("short_code", "original", "created_at").
+		From("urls").
+		Where(squirrel.Eq{"original": originalURL}).
+		ToSql()
+	if err != nil {
+		return nil, fmt.Errorf("failed to build query: %w", err)
+	}
+
+	var url model.URL
+	err = r.db.QueryRow(ctx, query, args...).Scan(&url.ShortCode, &url.Original, &url.CreatedAt)
+	if errors.Is(err, pgx.ErrNoRows) {
+		return nil, model.ErrURLNotFound
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute query: %w", err)
+	}
+
+	return &url, nil
+}
