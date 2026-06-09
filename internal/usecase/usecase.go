5941ad084d1b168018f17cd4389f87c13ba0fa06
Ivan12390123 <i_van_r@mail.ru>
2026-06-10 01:13:41 +0300

    add usecase: implementation of initial business logic

--- internal/usecase/usecase.go
+++ internal/usecase/usecase.go
@@ -2,44 +2,84 @@ package usecase
 
 import (
 	"context"
+	"errors"
+	"time"
 
 	"github.com/I-Van-Radkov/url-shortener/internal/model"
 )
 
 type Repository interface {
-	Save(ctx context.Context, url *model.URL) error
-	FindByOriginalURL(ctx context.Context, originalURL string) (*model.URL, error)
 	FindByShortCode(ctx context.Context, shortCode string) (*model.URL, error)
+	CreateOrFind(ctx context.Context, url *model.URL) (*model.URL, error)
+}
+
+type Generator interface {
+	Generate() (string, error)
 }
 
 type Usecase struct {
 	repo Repository
+	gen  Generator
+
+	maxAttemptsToGen int
 }
 
-func New(repo Repository) *Usecase {
-	return &Usecase{repo: repo}
+func New(repo Repository, gen Generator, maxAttemptsToGen int) *Usecase {
+	if maxAttemptsToGen <= 0 || maxAttemptsToGen > 5 {
+		maxAttemptsToGen = 5
+	}
+
+	return &Usecase{
+		repo:             repo,
+		gen:              gen,
+		maxAttemptsToGen: maxAttemptsToGen,
+	}
 }
 
-func (u *Usecase) SaveOriginalURL(ctx context.Context, originalUrl string) (*model.URL, error) {
-	// TODO: валидация
+func (u *Usecase) SaveOriginalURL(ctx context.Context, originalURL string) (*model.URL, error) {
+	err := model.ValidateOriginalURL(originalURL)
+	if err != nil {
+		return nil, err
+	}
 
-	// TODO: проверка на существование ссылки, если существует - возвращаем короткий код
+	now := time.Now().UTC()
 
-	// TODO: генерация короткого кода
+	for i := 0; i < u.maxAttemptsToGen; i++ {
+		code, err := u.gen.Generate()
+		if err != nil {
+			return nil, err
+		}
 
-	// TODO: проверка на существование короткого кода, если существует - перегенерируем
+		url := &model.URL{
+			ShortCode: code,
+			Original:  originalURL,
+			CreatedAt: now,
+		}
 
-	// TODO: сохраняем ссылку
+		url, err = u.repo.CreateOrFind(ctx, url)
+		if errors.Is(err, model.ErrShortCodeExists) {
+			continue
+		}
+		if err != nil {
+			return nil, err
+		}
 
-	// TODO: возвращаем результат
+		return url, nil
+	}
+
+	return nil, model.ErrGenerationFailed
 }
 
 func (u *Usecase) GetOriginalURL(ctx context.Context, shortCode string) (*model.URL, error) {
-	// TODO: валидация
+	err := model.ValidateShortCode(shortCode)
+	if err != nil {
+		return nil, err
+	}
 
-	// TODO: получение ссылки
+	url, err := u.repo.FindByShortCode(ctx, shortCode)
+	if err != nil {
+		return nil, err
+	}
 
-	// TODO: если ссылка не найдена, то возвращаем ошибку
-
-	// TODO: возвращаем результат
+	return url, nil
 }
