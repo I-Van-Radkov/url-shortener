289c888876d02a2dcdd194661e435f853e891d5d
Ivan12390123 <i_van_r@mail.ru>
2026-06-10 16:57:42 +0300

    add main functionality: initialize the config, create the application structure, and launch the application

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+
+	"github.com/I-Van-Radkov/url-shortener/internal/app"
+	"github.com/I-Van-Radkov/url-shortener/internal/config"
+)
+
+func main() {
+	cfg, err := config.ParseConfigFromEnv()
+	if err != nil {
+		log.Fatalf("config error: %v", err)
+	}
+
+	a, err := app.NewApp(cfg)
+	if err != nil {
+		log.Fatalf("app init error: %v", err)
+	}
+
+	if err = a.Run(); err != nil {
+		log.Fatalf("app run error: %v", err)
+	}
+}
