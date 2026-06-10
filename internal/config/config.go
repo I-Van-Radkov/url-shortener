4777753c77864f8d9f9d32536b565095f716fef9
Ivan12390123 <i_van_r@mail.ru>
2026-06-10 19:02:57 +0300

    fix internal/config: add validate config

--- internal/config/config.go
+++ internal/config/config.go
@@ -19,11 +19,32 @@ type Config struct {
 	GracefulShutdownTimeout time.Duration `env:"GRACEFUL_SHUTDOWN_TIMEOUT" env-default:"5s"`
 	StorageType             string        `env:"STORAGE_TYPE" env-default:"memory"`
 	MaxAttemptsToGen        int           `env:"MAX_ATTEMPTS_TO_GEN" env-default:"5"`
-	Port                    int           `env:"PORT" env-default:"8080"`
-	ReadTimeout             time.Duration `env:"HTTP_READ_TIMEOUT" env-default:"30s"`
-	WriteTimeout            time.Duration `env:"HTTP_WRITE_TIMEOUT" env-default:"30s"`
+
+	Port         int           `env:"PORT" env-default:"8080"`
+	ReadTimeout  time.Duration `env:"HTTP_READ_TIMEOUT" env-default:"30s"`
+	WriteTimeout time.Duration `env:"HTTP_WRITE_TIMEOUT" env-default:"30s"`
 
 	PostgresConfig PostgresConfig
+}
+
+func (c *Config) Validate() error {
+	if c.StorageType != "memory" && c.StorageType != "postgres" {
+		return fmt.Errorf("invalid STORAGE_TYPE: %s", c.StorageType)
+	}
+
+	if c.Port < 1 || c.Port > 65535 {
+		return fmt.Errorf("invalid PORT: %d", c.Port)
+	}
+
+	if c.MaxAttemptsToGen < 1 {
+		return fmt.Errorf("MAX_ATTEMPTS_TO_GEN must be between 1 and 10, got %d", c.MaxAttemptsToGen)
+	}
+
+	if c.GracefulShutdownTimeout < 1*time.Second {
+		return fmt.Errorf("GRACEFUL_SHUTDOWN_TIMEOUT must be at least 1s, got %v", c.GracefulShutdownTimeout)
+	}
+
+	return nil
 }
 
 func ParseConfigFromEnv() (*Config, error) {
@@ -31,6 +52,11 @@ func ParseConfigFromEnv() (*Config, error) {
 
 	if err := cleanenv.ReadEnv(cfg); err != nil {
 		return nil, fmt.Errorf("failed to parse config from env: %w", err)
+	}
+
+	err := cfg.Validate()
+	if err != nil {
+		return nil, fmt.Errorf("failed to validate config: %w", err)
 	}
 
 	return cfg, nil
